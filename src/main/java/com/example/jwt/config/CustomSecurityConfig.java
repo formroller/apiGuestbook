3a0641656f1df9cafdedb669ecbd85544ff4f355
formroller <54848570+formroller@users.noreply.github.com>
2024-02-20 15:13:00 +0900

    jwt - APILoginFIlter

--- src/main/java/com/example/jwt/config/CustomSecurityConfig.java
+++ src/main/java/com/example/jwt/config/CustomSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.jwt.config;
 
+import com.example.jwt.security.filter.APILoginFilter;
+import com.example.jwt.security.service.APIUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +18,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Log4j2
@@ -21,6 +26,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    private final APIUserDetailsService apiUserDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -39,6 +46,31 @@ public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception{
         log.info(" ============== configure ===========");
+
+        /*Authentication Manager 설정*/
+        // AuthenticationManager 설정
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                .userDetailsService(apiUserDetailsService)
+                        .passwordEncoder(passwordEncoder());
+
+        // GetAuthenticationManager
+        AuthenticationManager authenticationManager =
+                authenticationManagerBuilder.build();
+
+        // 반드시 필요
+        http.authenticationManager(authenticationManager);
+
+        // APILoginFilter
+        APILoginFilter apiLoginFilter = new APILoginFilter("/generateToken");
+        apiLoginFilter.setAuthenticationManager(authenticationManager);
+
+        // APILoginFilter 위치 조정
+        http.addFilterBefore(apiLoginFilter, UsernamePasswordAuthenticationFilter.class);
+        /* Authentication Manager 설정 종료*/
+
 
         // csrf 토큰 비활성화
         http.csrf(csrf -> csrf.disable());
